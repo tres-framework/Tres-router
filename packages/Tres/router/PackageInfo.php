c02ee94dcc96c95a0d705a65f52f52ada44f251b
pedzed <pedzed@outlook.com>
2014-10-28 23:30:25 +0100

    Added package info.

---
+++ packages/Tres/router/PackageInfo.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace packages\Tres\router {
+    
+    class PackageInfo {
+        
+        public static function get(){
+            return [
+                'version' => '0.1',
+                'authors' => [
+                    'pedzed' => [
+                        'profile' => 'https://github.com/pedzed/'
+                    ]
+                ]
+            ];
+        }
+        
+    }
+    
+}
